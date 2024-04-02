9079ffeb00f77684dd3be2fb832f82bf95e8b9e8
gugyeoj1n <kwooj2788@kookmin.ac.kr>
2024-04-02 10:02:39 +0900

    [FEAT] 서버 요청 중 로딩 패널 구현

--- Assets/Scripts/ServerManager.cs
+++ Assets/Scripts/ServerManager.cs
@@ -2,14 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
+using TMPro;
 
 public class ServerManager : MonoBehaviour
 {
     public string url;
+    public GameObject loadPanel;
+    private GameObject loadInstance;
     
     void Start()
     {
-        
+        SetLoadPanel("연결 확인 중");
     }
 
     IEnumerator Test()
@@ -17,11 +20,26 @@ public class ServerManager : MonoBehaviour
         UnityWebRequest request = UnityWebRequest.Get(url);
 
 
-        yield return request.SendWebRequest();  // 응답이 올때까지 대기한다.
+        yield return request.SendWebRequest();
 
-        if (request.error == null)  // 에러가 나지 않으면 동작.
+        if (request.error == null)
         {
             // text.text += request.downloadHandler.text;
         }
+    }
+
+    private void SetLoadPanel(string target)
+    {
+        TMP_Text text = loadPanel.transform.GetChild(1).GetComponent<TMP_Text>();
+        text.text = target;
+        Canvas canvas = FindObjectOfType<Canvas>();
+        loadInstance = Instantiate(loadPanel, canvas.transform);
+        Debug.Log("SET LOAD PANEL");
+    }
+
+    private void DestroyLoadPanel()
+    {
+        Destroy(loadInstance);
+        loadInstance = null;
     }
 }
