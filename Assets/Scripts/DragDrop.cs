0841010d438744b97287fe3313882fafc4e5cc10
gugyeoj1n <kwooj2788@kookmin.ac.kr>
2024-05-13 15:05:12 +0900

    [FEAT] 인게임 카메라 렌더 텍스쳐 연결

--- Assets/Scripts/DragDrop.cs
+++ Assets/Scripts/DragDrop.cs
@@ -4,7 +4,6 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using DG.Tweening;
-using UnityEditor.PackageManager.Requests;
 
 public class DragDrop : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
