39ee4da6ad1b27dbddc318b4ba4f57ce314c336a
gugyeoj1n <kwooj2788@kookmin.ac.kr>
2024-05-30 13:39:49 +0900

    [FEAT] 튜토리얼 씬 시퀀스 2 구현

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -11,6 +11,15 @@ public class TutorialManager : MonoBehaviour
     public GameObject textPanel;
     public TMP_Text tutorialText;
     public CinemachineVirtualCamera virtualCam;
+    public GameObject highlightIcon;
+    public Michsky.MUIP.ButtonManager playButton;
+
+    private Status status = Status.Seq1;
+
+    private enum Status
+    {
+        Seq1, Seq2, Seq3
+    }
     
     void Start()
     {
@@ -44,6 +53,69 @@ public class TutorialManager : MonoBehaviour
         {
             tutorialText.text = "";
             virtualCam.Priority = 9;
+            Seq2();
+        });
+        seq.Play();
+    }
+
+    public void Seq2()
+    {
+        status = Status.Seq2;
+        var seq = DOTween.Sequence();
+        seq.Append(tutorialText.DOText("코드 블록은 크게 Work, Control, Move 3가지 카테고리로 분류돼요.", 2f));
+        seq.AppendInterval(2f);
+        seq.AppendCallback(() =>
+        {
+            tutorialText.text = "";
+            highlightIcon.SetActive(true);
+        });
+        seq.Append(tutorialText.DOText("Work 블록은 제가 물건을 줍고 놓는 등의 행위를 나타내요.", 2f));
+        seq.AppendInterval(4f);
+        seq.AppendCallback(() =>
+        {
+            tutorialText.text = "";
+            RectTransform rect = highlightIcon.GetComponent<RectTransform>();
+            Vector2 newPosition = new Vector2(rect.anchoredPosition.x, rect.anchoredPosition.y - 80f);
+            highlightIcon.GetComponent<RectTransform>().anchoredPosition = newPosition;
+        });
+        seq.Append(tutorialText.DOText("Control 블록은 조건, 반복 등 상황에 맞게 응용할 수 있는 블록이에요.", 3f));
+        seq.AppendInterval(4f);
+        seq.AppendCallback(() =>
+        {
+            tutorialText.text = "";
+            RectTransform rect = highlightIcon.GetComponent<RectTransform>();
+            Vector2 newPosition = new Vector2(rect.anchoredPosition.x, rect.anchoredPosition.y - 80f);
+            highlightIcon.GetComponent<RectTransform>().anchoredPosition = newPosition;
+        });
+        seq.Append(tutorialText.DOText("Move 블록은 제가 걸어가거나 회전하는 이동 관련 행위를 나타내요.", 2f));
+        seq.AppendInterval(4f);
+        seq.AppendCallback(() =>
+        {
+            tutorialText.text = "";
+            highlightIcon.SetActive(false);
+        });
+        seq.Append(tutorialText.DOText("코드를 만드실 때는 원하는 블록을 오른쪽 빈 공간에 드래그한 후, 오른쪽 위 Play 버튼을 누르시면 돼요.", 4f));
+        seq.AppendInterval(4f);
+        seq.AppendCallback(() =>
+        {
+            tutorialText.text = "";
+        });
+        seq.Append(tutorialText.DOText("Move 카테고리의 걷기 블록을 1개만 사용해 볼까요?", 2f));
+        seq.AppendCallback(() =>
+        {
+            playButton.onClick.AddListener(() =>
+            {
+                if (FindObjectOfType<BlockManager>().listOfLists[0].blockList[0].GetComponent<Block>().blockData.name ==
+                    "Walk")
+                {
+                    Debug.Log("SEQ3 GOGO");
+                }
+                else
+                {
+                    tutorialText.text = "";
+                    tutorialText.DOText("Move 카테고리의 걷기 블록은 위 화살표 아이콘의 블록이에요.", 2f);
+                }
+            });
         });
         seq.Play();
     }
